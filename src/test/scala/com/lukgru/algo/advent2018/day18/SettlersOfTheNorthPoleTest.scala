61a8c5a56c8dcd71a2dfe75ad5eb82bfde42e126
lgruba <lukasz.gruba@sgdigital.com>
2018-12-18 12:28:21 +0100

    day18 - Settlers Of The North Pole - test for part 1

--- src/test/scala/com/lukgru/algo/advent2018/day18/SettlersOfTheNorthPoleTest.scala
+++ src/test/scala/com/lukgru/algo/advent2018/day18/SettlersOfTheNorthPoleTest.scala
@@ -1,5 +1,6 @@
 package com.lukgru.algo.advent2018.day18
 
+import com.lukgru.algo.advent2018.utils.InputLoader
 import org.scalatest.FunSuite
 
 class SettlersOfTheNorthPoleTest extends FunSuite {
@@ -24,6 +25,17 @@ class SettlersOfTheNorthPoleTest extends FunSuite {
 
     //then
     assert(solution == 1147)
+  }
+
+  test("should solve part 1") {
+    //given
+    val input = InputLoader.loadLines("day18-input")
+
+    //when
+    val solution = SettlersOfTheNorthPole.solvePart1(input)
+
+    //then
+    assert(solution == 480150)
   }
 
 }
