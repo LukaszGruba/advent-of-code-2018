cb58cad1a19e842f3b30261aae4e16a46dc4976b
LukaszGruba <lukgru@gmail.com>
2018-12-19 00:49:46 +0100

    day17 - Reservoir Research - part 1 done

--- src/main/scala/com/lukgru/algo/advent2018/day17/ReservoirResearch.scala
+++ src/main/scala/com/lukgru/algo/advent2018/day17/ReservoirResearch.scala
@@ -3,6 +3,8 @@ package com.lukgru.algo.advent2018.day17
 import com.lukgru.algo.advent2018.day17.ReservoirResearch.WaterState.WaterState
 import com.lukgru.algo.advent2018.utils.InputLoader
 
+import scala.collection.mutable
+
 object ReservoirResearch {
 
   object WaterState extends Enumeration {
@@ -38,15 +40,25 @@ object ReservoirResearch {
     lines.flatMap(parseLine).toSet
   }
 
-  def printClay(clay: Set[Position]): Unit = {
-    val xMin = clay.map(_.x).min
-    val xMax = clay.map(_.x).max
-    val yMin = clay.map(_.y).min
-    val yMax = clay.map(_.y).max
+  def printState(water: Map[Position, Water], clay: Set[Position]): Unit = {
+    val allPositions = water.map(_._2.pos) ++ clay
+    val xMin = allPositions.map(_.x).min
+    val xMax = allPositions.map(_.x).max
+    val yMin = allPositions.map(_.y).min
+    val yMax = allPositions.map(_.y).max
     val sb = new StringBuilder()
     for (y <- yMin to yMax) {
       for (x <- xMin to xMax) {
-        if (clay.contains(Position(x,y))) sb.append('#')
+        val p = Position(x, y)
+        if (clay.contains(p)) sb.append('#')
+        else if (water.contains(p)) {
+          val w = water(p)
+          val waterSign = w.state match {
+            case WaterState.Flowing => '|'
+            case WaterState.Still => '~'
+          }
+          sb.append(waterSign)
+        }
         else sb.append('.')
       }
       sb.append('\n')
@@ -54,15 +66,116 @@ object ReservoirResearch {
     println(sb.mkString)
   }
 
-  def solvePart1(lines: List[String]): Int = {
+  private def getPosBelow(p: Position): Position = p.copy(y = p.y + 1)
+
+  def isEndOfStream(w: Water, allTheWater: Map[Position, Water], clay: Set[Position]): Boolean = {
+    val posBelow = getPosBelow(w.pos)
+    w.state == WaterState.Flowing && !allTheWater.contains(posBelow) && !clay.contains(posBelow)
+  }
+
+  def isInTheStream(w: Water, water: Map[Position, Water], clay: Set[Position]): Boolean =
+    w.state == WaterState.Flowing && water.get(getPosBelow(w.pos)).exists(_.state == WaterState.Flowing)
+
+  def didHitStillElement(w: Water, water: Map[Position, Water], clay: Set[Position]): Boolean = {
+    val posBelow = getPosBelow(w.pos)
+    w.state == WaterState.Flowing && (water.get(posBelow).exists(_.state == WaterState.Still) || clay.contains(posBelow))
+  }
+
+  def expandWater(w: Water, water: Map[Position, Water], clay: Set[Position]): Set[Water] = {
+    def restrictedFromLeft(ws: Stream[Water]): Boolean = {
+      val leftMost = ws.map(_.pos).minBy(_.x)
+      clay.contains(leftMost.copy(x = leftMost.x - 1))
+    }
+
+    def restrictedFromRight(ws: Stream[Water]): Boolean = {
+      val rightMost = ws.map(_.pos).maxBy(_.x)
+      clay.contains(rightMost.copy(x = rightMost.x + 1))
+    }
+
+    def restrictedFromBothSides(ws: Stream[Water]): Boolean = {
+      restrictedFromLeft(ws) && restrictedFromRight(ws)
+    }
+
+    val waterToTheRight =
+      Stream.from(0)
+        .map(x => Position(w.pos.x + x, w.pos.y))
+        .takeWhile { p =>
+          !clay.contains(p) && (water.get(getPosBelow(p)).exists(_.state == WaterState.Still) || clay.contains(getPosBelow(p)))
+        }
+        .map(p => Water(p, WaterState.Flowing))
+    val waterToTheLeft =
+      Stream.from(0)
+        .map(x => Position(w.pos.x - x, w.pos.y))
+        .takeWhile { p =>
+          !clay.contains(p) && (water.get(getPosBelow(p)).exists(_.state == WaterState.Still) || clay.contains(getPosBelow(p)))
+        }
+        .map(p => Water(p, WaterState.Flowing))
+    val combined = waterToTheLeft ++ waterToTheRight
+    if (restrictedFromBothSides(combined)) {
+      combined.map(_.copy(state = WaterState.Still)).toSet
+    }
+    else {
+      val additionalOnLeft =
+        if (!restrictedFromLeft(combined)) {
+          val leftMost = combined.map(_.pos).minBy(_.x)
+          Some(Water(leftMost.copy(x = leftMost.x - 1), WaterState.Flowing))
+        } else None
+      val additionalOnRight =
+        if (!restrictedFromRight(combined)) {
+          val rightMost = combined.map(_.pos).maxBy(_.x)
+          Some(Water(rightMost.copy(x = rightMost.x + 1), WaterState.Flowing))
+        } else None
+      (Stream(additionalOnLeft, additionalOnRight).flatten ++ combined).toSet
+    }
+  }
+
+  //TODO: use water el -> many water els and flatmap to achieve immutability
+  def tick(water: Map[Position, Water], clay: Set[Position]): Map[Position, Water] = {
+    val newWater = mutable.Map.empty[Position, Water]
+    water.foreach { case (p, w) =>
+      w.state match {
+        case WaterState.Still => newWater.put(p, w)
+        case WaterState.Flowing =>
+          if (isEndOfStream(w, water, clay)) {
+            newWater.put(p, w)
+            val below = getPosBelow(p)
+            newWater.put(below, w.copy(pos = below))
+          } else if (isInTheStream(w, water, clay)) {
+            newWater.put(p, w)
+          } else if (didHitStillElement(w, water, clay)) {
+            expandWater(w, water, clay)
+              .foreach(newW => newWater.put(newW.pos, newW))
+          }
+      }
+    }
+    newWater.toMap
+  }
+
+  def reachedEnd(water: Map[Position, Water], clay: Set[Position]): Boolean = {
+    val bottom = clay.map(_.y).max
+    water.keys.map(_.y).exists(bottom.<=)
+  }
+
+  def solvePart1(springX: Int)(lines: List[String]): Int = {
     val clay = parseClay(lines)
-    printClay(clay)
-    ???
+    var water = Map(Position(springX, 0) -> Water(Position(springX, 0), WaterState.Flowing))
+    var i = 0
+    var lastWaterSize = 0
+    val top = clay.map(_.y).min
+    val bottom = clay.map(_.y).max
+    while (lastWaterSize != water.size) {
+      lastWaterSize = water.size
+      water = tick(water, clay).filter { case (p, _) => p.y <= bottom }
+      i += 1
+      println(i)
+    }
+    printState(water, clay)
+    water.count { case (p, _) => p.y >= top }
   }
 
   def main(args: Array[String]): Unit = {
     val input = InputLoader.loadLines("day17-input")
-    val numberOfReachedTiles = solvePart1(input)
+    val numberOfReachedTiles = solvePart1(500)(input)
     println(numberOfReachedTiles)
   }
 
--- src/test/scala/com/lukgru/algo/advent2018/day17/ReservoirResearchTest.scala
+++ src/test/scala/com/lukgru/algo/advent2018/day17/ReservoirResearchTest.scala
@@ -1,7 +1,38 @@
 package com.lukgru.algo.advent2018.day17
 
+import com.lukgru.algo.advent2018.utils.InputLoader
 import org.scalatest.FunSuite
 
 class ReservoirResearchTest extends FunSuite {
 
+  test("should solve simple example") {
+    //given
+    val input = List(
+      "x=495, y=2..7",
+      "y=7, x=495..501",
+      "x=501, y=3..7",
+      "x=498, y=2..4",
+      "x=506, y=1..2",
+      "x=498, y=10..13",
+      "x=504, y=10..13",
+      "y=13, x=498..504"
+    )
+
+    //when
+    val solution = ReservoirResearch.solvePart1(500)(input)
+
+    //then
+    assert(solution == 57)
+  }
+
+  test("should solve part 1") {
+    //given
+    val input = InputLoader.loadLines("day17-input")
+
+    //when
+    val solution = ReservoirResearch.solvePart1(500)(input)
+
+    //then
+    assert(solution == 33004)
+  }
 }
