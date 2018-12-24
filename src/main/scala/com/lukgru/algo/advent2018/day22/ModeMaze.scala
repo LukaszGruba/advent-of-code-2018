ee05e78f7341dc77c27e36581d6303c5767fd990
lgruba <lukasz.gruba@sgdigital.com>
2018-12-24 23:29:30 +0100

    day22 - Mode Maze - tests for part 1 + bugged attempt to solve part 2

--- src/main/scala/com/lukgru/algo/advent2018/day22/ModeMaze.scala
+++ src/main/scala/com/lukgru/algo/advent2018/day22/ModeMaze.scala
@@ -1,19 +1,30 @@
 package com.lukgru.algo.advent2018.day22
 
+import com.lukgru.algo.advent2018.day22.ModeMaze.Equipment.Equipment
 import com.lukgru.algo.advent2018.day22.ModeMaze.RegionType.RegionType
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 
 object ModeMaze {
 
-  val depth = 7740
-  val caveEntryPos = Position(0, 0)
-  val targetPos = Position(12, 763)
+  var depth = 0
+  var caveEntryPos = Position(0, 0)
+  var targetPos = Position(0, 0)
+
+  val changeCost = 7
 
   object RegionType extends Enumeration {
     type RegionType = Value
     val Rocky, Narrow, Wet = Value
   }
+
+  object Equipment extends Enumeration {
+    type Equipment = Value
+    val Neither, ClimbingGear, Torch = Value
+  }
+
+  case class TripState(time: Int, path: Vector[Position], currentEquipment: Equipment)
 
   case class Position(x: Int, y: Int)
 
@@ -22,8 +33,8 @@ object ModeMaze {
   }
 
   def calcGeologicIndex(regPos: Position): Long = regPos match {
-    case `caveEntryPos` => 0
-    case `targetPos` => 0
+    case _ if regPos == caveEntryPos => 0
+    case _ if regPos == targetPos => 0
     case Position(x, 0) => x * 16807
     case Position(0, y) => y * 48271
     case Position(x, y) => calcErosionLevel(Position(x - 1, y)) * calcErosionLevel(Position(x, y - 1))
@@ -43,7 +54,7 @@ object ModeMaze {
       cache.getOrElse(pos, {
         cache.update(pos, calcErosionLevel(pos))
         val erosionLevel = cache(pos)
-        println(s"cache updated for: ($pos, $erosionLevel")
+        println(s"cache updated with: ($pos, $erosionLevel)")
         erosionLevel
       })
   }
@@ -67,7 +78,11 @@ object ModeMaze {
     area.toList
   }
 
-  def solvePart1(): Long = {
+  def solvePart1(depthP: Int, entrance: Position, target: Position): Long = {
+    this.depth = depthP
+    this.caveEntryPos = entrance
+    this.targetPos = target
+
     val areaToCalc = getArea(caveEntryPos, targetPos)
     areaToCalc.map(calcErosionLevel)
       .map(determineRegionType)
@@ -75,9 +90,61 @@ object ModeMaze {
       .sum
   }
 
+  def adjacentPos(pos: Position): List[Position] = {
+    val Position(x, y) = pos
+    List(
+      Position(x + 1, y),
+      Position(x - 1, y),
+      Position(x, y + 1),
+      Position(x, y - 1)
+    ).filter(p => p.x >= 0 && p.y >= 0)
+  }
+
+  def createNewPaths(current: TripState): Vector[TripState] = {
+    val adjacentPositions = adjacentPos(current.path.last)
+    val possibleMoves = for (pos <- adjacentPositions; possibleEq <- Equipment.values.toVector) yield (pos, possibleEq)
+    possibleMoves.map { case (newPos, eq) =>
+      val path = current.path :+ newPos
+      val time = if (eq != current.currentEquipment) {
+        current.time + 1 + changeCost
+      } else current.time + 1
+      TripState(time, path, eq)
+    }.toVector
+  }
+
+  @tailrec
+  def findShortestTrip(tripStates: Vector[TripState]): TripState = {
+    if (tripStates.size % 1000 == 0) println(tripStates.size)
+    val (head, tail) = (tripStates.head, tripStates.tail)
+    if (head.path.last == targetPos) {
+      if (head.currentEquipment == Equipment.Torch) head
+      else {
+        val newTail = TripState(head.time + changeCost, head.path, Equipment.Torch) +: tail
+        findShortestTrip(newTail.sortBy(_.time))
+      }
+    }
+    else {
+      val newPaths = createNewPaths(head)
+      val newTail = newPaths ++ tail
+      findShortestTrip(newTail.sortBy(_.time))
+    }
+  }
+
+  def solvePart2(): Long = {
+    val TripState(time, _, _) = findShortestTrip(Vector(TripState(0, Vector(caveEntryPos), Equipment.Torch)))
+    time
+  }
+
   def main(args: Array[String]): Unit = {
-    val solution1 = solvePart1()
+    val depthP = 7740
+    val caveEntryPosP = Position(0, 0)
+    val targetPosP = Position(12, 763)
+
+    val solution1 = solvePart1(depthP, caveEntryPosP, targetPosP)
     println(solution1)
+
+    val solution2 = solvePart2()
+    println(solution2)
   }
 
 }
--- src/test/scala/com/lukgru/algo/advent2018/day22/ModeMazeTest.scala
+++ src/test/scala/com/lukgru/algo/advent2018/day22/ModeMazeTest.scala
@@ -1,11 +1,34 @@
 package com.lukgru.algo.advent2018.day22
 
+import com.lukgru.algo.advent2018.day22.ModeMaze.Position
 import org.scalatest.FunSuite
 
 class ModeMazeTest extends FunSuite {
 
+  test("should solve for simple example"){
+    //given
+    val depth = 510
+    val entrance = Position(0, 0)
+    val target = Position(10, 10)
+
+    //when
+    val solution = ModeMaze.solvePart1(depth, entrance, target)
+
+    //then
+    assert(solution == 114)
+  }
+
   test("should solve part 1") {
-    assert(ModeMaze.solvePart1() == 9899)
+    //given
+    val depthP = 7740
+    val caveEntryPosP = Position(0, 0)
+    val targetPosP = Position(12, 763)
+
+    //when
+    val solution = ModeMaze.solvePart1(depthP, caveEntryPosP, targetPosP)
+
+    //then
+    assert(solution == 9899)
   }
 
 }
